2ab24ee3a1731f593a367bd0c0dac01c715f958c
thekthuser <thekthuser@thekthuser.com>
2018-10-22 12:26:56 -0400

    setup UserDAO in main.go

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	_ "mealplans/routers"
 	"github.com/astaxie/beego"
+  "mealplans/dao"
 )
 
+var udao = dao.UserDAO{}
+
 func main() {
+  udao.Server = "localhost"
+  udao.Database = "mealplans_db"
+  udao.Connect()
 	beego.Run()
 }
 
