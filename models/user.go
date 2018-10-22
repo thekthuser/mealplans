369df285a795e2dec970e7570267ddbc8492cff4
thekthuser <thekthuser@thekthuser.com>
2018-10-22 11:29:22 -0400

    create User struct

---
+++ models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import {
+  "fmt"
+  "gopkg.in/mgo.v2/bson"
+}
+
+type User struct {
+  id bson.ObjectId `bson:"_id" json:"id"`
+  mealPlanId `bson:"mealPlanId" json:"mealPlanId"`
+  username string `bson:"username" json:"username"`
+  password string `bson:"password" json:"password"`
+  name string `bson:"name" json:"name"`
+  school string `bson:"school" json:"school"`
+}
