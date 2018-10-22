a00f649cb42294c9a55fc6b0996a6e8df798fbd0
thekthuser <thekthuser@thekthuser.com>
2018-10-22 14:57:59 -0400

    adjust/add fields to User, Plan

--- models/plans.go
+++ models/plans.go
@@ -7,9 +7,14 @@ import (
 type Plan struct {
   Id bson.ObjectId `bson:"_id" json:"id"`
   Name string `bson:"name" json:"name"`
-  Cost string `bson:"cost" json:"cost"`
+  Cost int `bson:"cost" json:"cost"`
   Market string `bson:"market" json:"market"`
-  //Semester1Start string `bson:"semester1start" json:"semester1start"`
+  Semester1Start string `bson:"semester1start" json:"semester1start"`
+  Semester1End string `bson:"semester1end" json:"semester1end"`
+  Semester2Start string `bson:"semester2start" json:"semester2start"`
+  Semester2End string `bson:"semester2end" json:"semester2end"`
+  Semester3Start string `bson:"semester3start" json:"semester3start"`
+  Semester3End string `bson:"semester3end" json:"semester3end"`
   MarketingText1 string `bson:"marketingtext1" json:"marketingtext1"`
   MarketingText2 string `bson:"marketingtext2" json:"marketingtext2"`
   MarketingText3 string `bson:"marketingtext3" json:"marketingtext3"`
--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
   Id bson.ObjectId `bson:"_id" json:"id"`
-  MealPlanId string `bson:"mealPlanId" json:"mealPlanId"`
+  MealPlanId bson.ObjectId `bson:"mealPlanId" json:"mealPlanId"`
   Username string `bson:"username" json:"username"`
   Password string `bson:"password" json:"password"`
   Name string `bson:"name" json:"name"`
